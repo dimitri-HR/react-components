776f904ea3e71572bb488b78593179239805ed3a
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-25 10:55:37 +1100

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// TODO
+var GroceryList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <ul>
+        { props.items.map(item =>
+          <GroceryListItem item={item}/>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+var groceryItems = ['flour', 'egs', '2juice'];
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bold: false
+    };
+  }
+
+  onMouseOver() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+    return <li style={style} onMouseOver={this.onMouseOver.bind(this)}>{this.props.item}</li>;
+  }
+}
+
+
+ReactDOM.render(<GroceryList items={groceryItems} />, document.getElementById('app'));
